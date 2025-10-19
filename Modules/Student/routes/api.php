3f0c2135a87b239af3aa41591a6d7f751135d77a
amirhosseinAl <amirh.arab80@gmail.com>
2025-10-19 12:56:44 +0330

    Delete Modules/Student directory

--- Modules/Student/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Student\App\Http\Controllers\StudentController;
-
-Route::middleware(['Authentication'])->group(function () {
-    Route::post('/courses/register', [StudentController::class, 'Register']);
-    Route::get('/mycourses', [StudentController::class, 'StudentCourses']);
-});
