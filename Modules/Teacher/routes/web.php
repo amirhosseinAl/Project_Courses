b13a2dd6058e62bbc73fda9bfcc53121554a9bb5
amirhosseinAl <amirh.arab80@gmail.com>
2025-10-19 12:40:08 +0330

    Delete Modules/Teacher directory

--- Modules/Teacher/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Teacher\Http\Controllers\TeacherController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('teachers', TeacherController::class)->names('teacher');
-});
